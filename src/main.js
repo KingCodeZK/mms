9e256501f355990949c7fcb0c67db8d3e4f93dda
k_wing <18053255962@163.com>
2020-09-17 21:10:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'// 组件库
+import 'element-ui/lib/theme-chalk/index.css' // 样式
 import App from './App.vue'
+//import router from './router'
 
+// 使用ElementUI 
+Vue.use(ElementUI);
+
+//消息提示的环境配置，是否为生产环境：
+//flase 开发环境：Vue会提供很多警告来方便调试代码
+//true  生产环境：警告却没有用，反而会增加应用的体积
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
